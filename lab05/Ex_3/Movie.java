834f75229a07fe09f0d6dc2d136ee8a2d72bf0be
goncalo-machado <goncalofmachado01@gmail.com>
2021-04-26 14:10:25 +0100

    Teste

---
+++ lab05/Ex_3/Movie.java
@@ -0,0 +1,17 @@
+public class Movie {
+    private final String title;
+    private final int year;
+    private final Person director;
+    private final Person writer;
+    private final String series;
+    private final List<Person> cast;
+    private final List<Place> locations;
+    private final List<String> languages;
+    private final boolean isTelevision;
+    private final boolean isNetflix;
+    private final boolean isIndependent;
+
+    public static class Builder{
+        private final int 
+    }
+}
