cd5bd86e82d7054390c7e75e0b188c2c2431dd31
Rodrigo <rodrigoflima@ua.pt>
2021-05-02 11:39:16 +0100

    Finished alinea 2 of EX1

---
+++ lab06/Ex_1/Adapter.java
@@ -0,0 +1,59 @@
+// Adapter
+class Adapter {
+    private Database database;
+    private Registos registos;
+
+    // Adapter constructor
+    public Adapter(Database database, Registos registos){
+        this.database = database;
+        this.registos = registos;
+    }
+
+    // Saves employee on the databases
+    public void save(Object person){
+        if (person instanceof Employee){
+            database.addEmployee((Employee) person);
+        }
+        if (person instanceof Empregado){
+            registos.insere((Empregado) person);
+        }
+    }
+
+    // Removes employee from databases
+    public void remove(Object emp_num){
+        if (emp_num instanceof Integer){
+            registos.remove((int) emp_num);
+        }
+        if (emp_num instanceof Long){
+            database.deleteEmployee((long) emp_num);
+        }
+    }
+
+    // Checks if the employee is in our database's
+    public boolean belongs(Object emp_num){
+        if (emp_num instanceof Integer){
+            return registos.isEmpregado((int) emp_num);
+        }
+        if (emp_num instanceof Long){
+            Employee[] employees = database.getAllEmployees(); 
+
+            for (int i = 0; i < employees.length; i++)
+                if((long) emp_num == employees[i].getEmpNum()) return true;
+            return false;
+        }
+        return false;
+    }
+
+    // Prints our databases 
+    public void show(){
+        // Print Employees
+        for (Employee emp : database.getAllEmployees()){
+            System.out.printf("(%s, %d, %.2f)\n",emp.getName(),emp.getEmpNum(),emp.getSalary());
+        }
+
+        // Print registos
+        for (Empregado emp : registos.listaDeEmpregados()){
+            System.out.printf("(%s %s, %d, %.2f)\n",emp.nome(),emp.apelido(),emp.codigo(), emp.salario());
+        }
+    }
+}
---
+++ lab06/Ex_1/Alinea2.java
@@ -0,0 +1,71 @@
+class Alinea2{
+    public static void main(String args[]){
+        // Create Database
+        System.out.println("Creating database");
+        Database database = new Database();
+        System.out.println("DONE\n");
+        
+        // Create Registos
+        System.out.println("Creating registos");
+        Registos registos = new Registos();
+        System.out.println("DONE\n");
+
+        // Create Adapter
+        System.out.println("Creating adapter");
+        Adapter adapter = new Adapter(database, registos);
+        System.out.println("DONE\n");
+
+        // Create Employees
+        System.out.println("Creating employees");
+        Employee Jose = new Employee("Jose Andre",  98374, 1600);
+        Employee Ana  = new Employee("Ana Banana",  98375, 1700);
+        System.out.println("DONE\n");
+
+        // Create Empregados
+        System.out.println("Creating empregados");
+        Empregado Joao = new Empregado("Joao","Beirao", 98376, 1800);
+        Empregado Maria = new Empregado("Maria","Sofia",98377, 1900);
+        System.out.println("DONE\n");
+        
+        // Add persons to databases
+        System.out.println("Adding persons");
+        adapter.save(Jose);
+        adapter.save(Ana);
+        adapter.save(Joao);
+        adapter.save(Maria);
+        System.out.println("DONE\n");
+
+        // Check if employes belong to company
+        System.out.println("Checking belonging persons");
+        System.out.print("The person with the number 98372");
+        if(adapter.belongs(98372)){
+            System.out.print(" belongs to the company.\n");
+        }else{
+            System.out.print(" doenst belong to the company.\n");
+        }
+
+        System.out.print("The person with the number 98376");
+        if(adapter.belongs(98376)){
+            System.out.print(" belongs to the company.\n");
+        }else{
+            System.out.print(" doenst belong to the company.\n");
+        }
+        System.out.println("DONE\n");
+
+        // Print Databases
+        System.out.println("Printing Databases");
+        adapter.show();
+        System.out.println("DONE\n");
+
+        // Remove two persons
+        System.out.println("Removing persons");
+        adapter.remove((long)98375);
+        adapter.remove(98376);
+        System.out.println("DONE\n");
+        
+        // Print Databases
+        System.out.println("Printing Databases");
+        adapter.show();
+        System.out.println("DONE\n");
+    }
+}
